0d061437c2f29c6d8e589e0bd10345946e88458b
ttsamanemashaba004 <ttsamane.mashaba@gmail.com>
2025-01-16 11:01:21 +0200

    player1 and player2

--- src/contexts/GameContext.jsx
+++ src/contexts/GameContext.jsx
@@ -8,14 +8,14 @@ const GameContextProvider = (props) => {
     board: [null, null, null, null, null, null, null, null, null],
     player1: {
       choice: "x",
-      name: "Tinyiko",
+      name: "Player1",
       score: 0,
       color: "#8437f9",
       avatarConfig: genConfig(),
     },
     player2: {
       choice: "o",
-      name: "Harjot",
+      name: "Player2",
       score: 0,
       color: "#f9c811",
       avatarConfig: genConfig(),
